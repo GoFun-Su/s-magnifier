a032da2a0cbb0e13ee1faa6a01237f24116362d1
suli <sulideer@163.com>
2017-04-07 17:10:46 +0800

    格式问题

--- magnifier.js
+++ magnifier.js
@@ -2,36 +2,36 @@
   var Magnifier = function(opt) {
       var self=this;
       self.setting = {
-        fixed_one:"",
-        fixed_two:"",
-        big_img:"",
-        small_width:0,
-        small_height:0,
-        big_width:0,
-        big_height:0,
-        mouse_width:0,
-        mouse_height:0,
-        img_list:[],
-        small_list:[],
-        big_list:[]
+        left_fixed_box:"",
+       right_fixed_box:"",
+               big_img:"",
+           small_width:0,
+          small_height:0,
+             big_width:0,
+            big_height:0,
+           mouse_width:0,
+          mouse_height:0,
+              img_list:[],
+            small_list:[],
+              big_list:[]
       };
       self.config=$.extend({},self.setting, opt);
       self.render();
       $(document).mousemove(function(event) {
          var _self=this;
-         var x=event.pageX-$(self.config.fixed_one).offset().left-self.config.mouse_width/2;
-         var y=event.pageY-$(self.config.fixed_one).offset().top-self.config.mouse_height/2;
-         self.setPos(x,y,event.pageX,event.pageY,$(self.config.fixed_one).offset().left,$(self.config.fixed_one).offset().top)
+         var x=event.pageX-$(self.config.left_fixed_box).offset().left-self.config.mouse_width/2;
+         var y=event.pageY-$(self.config.left_fixed_box).offset().top-self.config.mouse_height/2;
+         self.setPos(x,y,event.pageX,event.pageY,$(self.config.left_fixed_box).offset().left,$(self.config.left_fixed_box).offset().top)
       })
     }
 Magnifier.prototype.render=function(){
   var self=this;
   //生成节点
-  $(self.config.fixed_one).append("<img src='"+self.config.small_img_src+"' class='small'>");
-  $(self.config.fixed_one).append("<span style='display:none;'></span>");
+  $(self.config.left_fixed_box).append("<img src='"+self.config.small_img_src+"' class='small'>");
+  $(self.config.left_fixed_box).append("<span style='display:none;'></span>");
   var $bigWrap=$("<div class='bigWrap' style='display:none;'></div>");
   var $photo=$("<div class='bigPhoto'></div>");
-  $(self.config.fixed_one).append($bigWrap.html($photo));
+  $(self.config.left_fixed_box).append($bigWrap.html($photo));
   self.setImageParam(self.config.small_img_src);
   self.setImageParam(self.config.big_img_src);//不用renderImage，直接一步到setImageParam，是因为图片加载是异步，返回的数据有误，所以去掉返回结果
 }
@@ -60,20 +60,20 @@ Magnifier.prototype.renderCss=function(){
   self.config.mouse_height=self.config.small_height*self.config.small_height/self.config.big_height;
 
   //计算节点的宽和高
-  $(self.config.fixed_one).css({
+  $(self.config.left_fixed_box).css({
     width:self.config.small_width+"px",
     height:self.config.small_height+"px"
   });
-  $(self.config.fixed_one).find("span").css({
+  $(self.config.left_fixed_box).find("span").css({
     width:self.config.mouse_width+"px",
     height:self.config.mouse_height+"px"
   });
-  $(self.config.fixed_one).find(".bigWrap").css({
+  $(self.config.left_fixed_box).find(".bigWrap").css({
     width:self.config.small_width+"px",
     height:self.config.small_height+"px",
     right:"-"+(parseInt(self.config.small_width)+10)+"px"
   });
-  $(self.config.fixed_one).find(".bigPhoto").css({
+  $(self.config.left_fixed_box).find(".bigPhoto").css({
     width:self.config.small_width+"px",
     height:self.config.small_height+"px",
     "background-position": "0 0",
@@ -106,17 +106,17 @@ Magnifier.prototype.setImageParam=function(src){
 }
 Magnifier.prototype.setPos=function(x,y,pageX,pageY,l,t){
   if (pageX<l || (l+this.config.small_width)<pageX) {
-    $(this.config.fixed_one).find("span").hide();
-    $(this.config.fixed_one).find(".bigWrap").hide();
+    $(this.config.left_fixed_box).find("span").hide();
+    $(this.config.left_fixed_box).find(".bigWrap").hide();
     return;
   }
   if (pageY<t || (t+this.config.small_height)<pageY) {
-    $(this.config.fixed_one).find("span").hide();
-    $(this.config.fixed_one).find(".bigWrap").hide();
+    $(this.config.left_fixed_box).find("span").hide();
+    $(this.config.left_fixed_box).find(".bigWrap").hide();
     return;
   }
-  $(this.config.fixed_one).find("span").show();
-  $(this.config.fixed_one).find(".bigWrap").show();
+  $(this.config.left_fixed_box).find("span").show();
+  $(this.config.left_fixed_box).find(".bigWrap").show();
     if(x<0){
       x=0;
     }
@@ -129,7 +129,7 @@ Magnifier.prototype.setPos=function(x,y,pageX,pageY,l,t){
     if(y>this.config.small_height-this.config.mouse_height){
       y=this.config.small_height-this.config.mouse_height;
     };
-    $(this.config.fixed_one).find("span").css({
+    $(this.config.left_fixed_box).find("span").css({
       left: x,
       top: y
     });
@@ -137,7 +137,7 @@ Magnifier.prototype.setPos=function(x,y,pageX,pageY,l,t){
     // this.config.small_width/(this.config.small_width-this.config.mouse_width)*x*(this.config.big_width/this.config.small_width);
     var pos_w=(this.config.big_width-this.config.small_width)/(this.config.small_width-this.config.mouse_width)*x;
     var pos_h=(this.config.big_height-this.config.small_height)/(this.config.small_height-this.config.mouse_height)*y;
-    $(this.config.fixed_one).find(".bigPhoto").css({
+    $(this.config.left_fixed_box).find(".bigPhoto").css({
       "background-position": "-"+pos_w+"px -"+pos_h+"px",
     });
 }
